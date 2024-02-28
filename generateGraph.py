1fc31086f1ef5c9c6251f0a12e444f1429e62637
Alex29f <106327042+Alex29f@users.noreply.github.com>
2024-02-28 18:01:39 +0200

    fix to sometimes having 6edges for 1 vertex

--- generateGraph.py
+++ generateGraph.py
@@ -4,9 +4,6 @@ import random
 def verticeLetters(verticeCount):
     return [chr(65 + i) for i in range(verticeCount)]
 
-def verticeLetters(verticeCount):
-    return [chr(65 + i) for i in range(verticeCount)]
-
 def generateGraph(verticeCount):
     G = nx.Graph()
     vertices = verticeLetters(verticeCount)
@@ -18,7 +15,7 @@ def generateGraph(verticeCount):
     for node in vertices:
         while len(G.edges(node)) < 2 or (len(G.edges(node)) < 5 and random.random() > 0.5):
             target = random.choice(vertices)
-            if target != node and not G.has_edge(node, target):
+            if target != node and not G.has_edge(node, target) and len(G.edges(target)) < 5:
                 G.add_edge(node, target, weight=random.randint(1, 10))
 
     return G
